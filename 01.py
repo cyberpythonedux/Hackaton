e3c2d16bda6261637c9437d7618fee7abc71ebcc
cyberpythonedux <cyberpythonedux@gmail.com>
2024-12-13 10:43:09 -0400

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,18 +1,18 @@
 from datetime import datetime
-
+import random
 def login():
     cpf_requerido = input("Digite seu CPF: ")
     senha_requerida = input("Digite sua senha: ")
     encontrou = False
 
-    for nome, dados in usuarios.items():
-        if cpf_requerido in dados['cpf']:
+    for cpf, dados in usuarios.items():
+        if cpf_requerido in cpf:
             encontrou = True
             if dados['senha'] == senha_requerida:
-                print(f"Bem-vindo(a) {nome}, login com sucesso!")
-                return nome,True  # Retorna que o login foi bem-sucedido
+                print(f"Bem-vindo(a) {dados['nome']}, login com sucesso!")
+                return cpf,True  # Retorna que o login foi bem-sucedido
             else:
-                print(f"Senha incorreta para o usuário {nome}.")
+                print(f"Senha incorreta para o usuário {cpf}.")
                 return False  # Retorna que a senha está incorreta
     if not encontrou:
         print("Usuário não encontrado.")
@@ -104,27 +104,45 @@ def menu_interno(nome_cliente):
             print(usuarios[nome_cliente]['saldo'])
         elif menu_escolha == 2:
             valor_deposito = float(input("Informe a quantidade do deposito: "))         
-            usuarios[nome_cliente]['saldo'] = valor_deposito
+            usuarios[nome_cliente]['saldo'] += valor_deposito
+            usuarios[nome_cliente]['operacao_deposito'].append(valor_deposito)
+            #arrumar para ficar mais legivel
+            usuarios[nome_cliente]['data_deposito'].append(datetime.now().date())
+            #apenas dias
+            print((usuarios[nome_cliente]['data_deposito'][0] - datetime(2024, 11, 1).date()).days)         
             print("Desposito realizado com sucesso")
             #print(usuarios[nome_cliente])
         elif menu_escolha == 3:
             encontrou = False
             agencia_transferencia = input("Informe a agência de destino")
             conta_transferencia = input("Informe a conta de destino")
-            for nome, dados in usuarios.items():
-                if agencia_transferencia in dados['agencia']:
-                    if conta_transferencia in dados['conta']:
-                        encontrou = True
-                        print(f'encontru {nome}')
-            if not encontrou:
-                print("Usuário não encontrado.")
-            return False  # Retorna que o usuário não foi encontrado
+            if usuarios[nome_cliente]['conta'] != conta_transferencia: 
+                valor_transferencia = float(input("Informe o valor da transferência: "))
+                if usuarios[nome_cliente]['saldo'] > valor_transferencia:
+                    for nome, dados in usuarios.items():
+                        if agencia_transferencia in dados['agencia']:
+                            if conta_transferencia in dados['conta']:
+                                encontrou = True
+                                usuarios[nome_cliente]['saldo'] += -valor_transferencia 
+                                usuarios[nome_cliente]['operacao_transferencia'].append(-valor_transferencia)
+                                usuarios[nome_cliente]['data_transferencia'].append(datetime.now().date())
+                                #ele volta ao menu e não ao menu interno
+                                print(f'encontru {nome}')
+                    if not encontrou:
+                        print("Usuário não encontrado. Verifique a agência e conta!")
+                    return False  # Retorna que o usuário não foi encontrado
+                else:
+                    print("Valor insuficiente para transferência!")
+            else:
+                print("Operação inválida")
 
             
         elif menu_escolha == 4:
             valor_saque=float(input("Digite o valor para sacar:  "))
             if valor_saque <= usuarios[nome_cliente]['saldo']:
                 usuarios[nome_cliente]['saldo'] += -valor_saque
+                usuarios[nome_cliente]['operacao_saque'].append(-valor_saque)
+                usuarios[nome_cliente]['data_saque'].append(datetime.now().date())
                 print("Saque efetuado com sucesso!")
             else:
                 print("Valor não disponivel.")
@@ -190,26 +208,49 @@ def menu():
                     elif acao == "sair":
                         return  # Encerra o programa
         elif escolha_menu == 2:
+            #ajustar nome completo
             nome = input("Digite seu nome completo: ")
             cpf = input("Digite seu CPF: ")
             # Valida o CPF digitado
             if valida_cpf(cpf):
                 print(f"O CPF {cpf} é válido.")
+                #melhorar o input, mais claro
                 data_nascimento=input("Digite sua data de nascimento: ")
                 valido, mensagem = valida_data_nascimento(data_nascimento)
                 if valido:
                     print(mensagem)
-                    agencia = input("Informe sua agência: ")
-                    conta = input("Informe sua conta: ")
+                    #agencia = input("Informe sua agência: ")
+                    agencia_lista= [11111,22222,3333,44444,5555]
+                    numero_aleatorio = random.randint(0,4)
+                    agencia = str(agencia_lista[numero_aleatorio])
+                    print(agencia)
+                    #conta = input("Informe sua conta: ")
+                    conta = random.sample(range(10),5)
+                    conta = "".join(map(str,conta))
                     
+                    """
+                    for cpf,dados in usuarios.items():
+                        while conta == dados['conta']:
+                            conta = random.sample(range(10),5)
+                            conta = "".join(map(str,conta))
+                    """
+                    
+                        
+                    print(conta)
                     senha = input("Digite sua senha: ")
-                    usuarios[nome] = {
-                    "cpf": cpf,
+                    usuarios[cpf] = {
+                    "nome": nome,
                     "senha": senha,
                     "data_nascimento": data_nascimento,
                     "saldo": 0,
                     "agencia": agencia,
                     "conta": conta,
+                    "operacao_deposito": [],
+                    "data_deposito": [],
+                    "operacao_transferencia": [],
+                    "data_transferencia": [],
+                    "operacao_saque": [],
+                    "data_saque":[],
                 
                     }
                     print("Cadastro realizado com sucesso!")
@@ -223,9 +264,6 @@ def menu():
                 print(f"O CPF {cpf} é inválido.")
                 
 
-            
-           
-            
             
         elif escolha_menu == 3:
             print("Por favor, faça login para trocar de usuário.")
@@ -241,24 +279,37 @@ usuarios = {}
 
 
 usuarios = {
-    "João Silva": {
-        "cpf": "12345678909",  # CPF válido
+    "12345678909": {
+        "nome": "joão silva",  # CPF válido
         "senha": "senha123",  # Senha válida
         "data_nascimento": "15/08/1990",  # Data de nascimento válida
         "saldo": 500.0,  # Saldo inicial de 500
         "agencia": '1234',
         "conta": '123',
+        "operacao_deposito": [],
+        "data_deposito": [],
+        "operacao_transferir": [],
+        "data_transferencia": [],
+        "operacao_sacar": [],
+        "data_saque":[],
+        
     },
-    "Maria Oliveira": {
-        "cpf": "98765432100",  # CPF válido
+    "98765432100": {
+        "nome": "Maria Silva da Silva",  # CPF válido
         "senha": "maria2023",  # Senha válida
         "data_nascimento": "20/05/1985",  # Data de nascimento válida
         "saldo": 1200.0,  # Saldo inicial de 1200
         "agencia": '321',
         "conta": '3214',
+        "operacao_deposito": [],
+        "data_deposito": [],
+        "operacao_transferencia": [],
+        "data_transferencia": [],
+        "operacao_saque": [],
+        "data_saque":[],
+        
     }
 }
 
 menu()
-
 
